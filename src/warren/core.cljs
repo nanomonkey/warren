b24f103ff0a71b17af7c92e344e131346de626b0
Scott Garrison <scottrobertgarrison@gmail.com>
2017-08-12 20:01:52 -0700

    Initial commit

---
+++ src/warren/core.cljs
@@ -0,0 +1,143 @@
+(ns warren.core
+  (:require [reagent.core :as reagent :refer [atom]] 
+            [clojure.string :as str]))
+
+(enable-console-print!) 
+
+(defn new-board [i j]
+  (vec (repeat j (vec (repeat i "0")))))
+  
+(defonce board-size [30 20])
+(defonce initial-position [2 2])
+(defonce state (atom {:text "Welcome to Warren"
+                      :board (apply new-board board-size)
+                      ;;:position (zipmap [:x :y] initial-position)
+                      :x (first initial-position)
+                      :y (second initial-position)}))
+
+(defn can-move? [x y direction]
+  (and (>= x 0) 
+       (<= x (first board-size))
+       (>= y 0)
+       (<= y (second board-size))
+       (not (str/includes? (get-in @state [:board x y]) direction))))
+
+(defn move-character! [direction]
+  (let [x (:x @state)
+        y (:y @state)]
+    (if (can-move? x y direction)
+        (case direction
+          "n" (swap! state assoc :y (dec y))
+          "s" (swap! state assoc :y (inc y))
+          "e" (swap! state assoc :x (inc x))
+          "w" (swap! state assoc :x (dec x))))))
+
+(defn handle-keys! [event]
+  (let [key (.-keyCode event)]
+    (case key
+          32 (println "spacebar")                            ; spacebar
+          13 (println "enter")                               ; enter
+          37 (move-character! "w")
+          38 (move-character! "n")
+          39 (move-character! "e")
+          40 (move-character! "s")
+          (println key))))
+
+(defn circle [x y]
+  [:circle
+   {:r 0.1
+    :stroke "blue"
+    :stroke-width 0.5
+    :fill "none"
+    :cx (+ 0.5 x)
+    :cy (+ 0.5 y)}])
+
+(defn border-top [x y]
+  [:line {:stroke "black"
+          :stroke-width 0.1
+          :x1 x
+          :x2 (+ x 1) 
+          :y1 y
+          :y2 y}])
+
+(defn border-bottom [x y]
+  [:line {:stroke "black"
+          :stroke-width 0.1
+          :x1 (+ x 1) 
+          :x2 (+ x 1) 
+          :y1 y
+          :y2 y}])
+
+(defn border-right [x y]
+  [:line {:stroke "black"
+          :stroke-width 0.1
+          :x1 x
+          :x2 x 
+          :y1 y
+          :y2 (+ y 1)}])
+
+(defn border-left [x y]
+  [:line {:stroke "black"
+          :stroke-width 0.1
+          :x1 (+  x 1) 
+          :x2 (+  x 1) 
+          :y1 y
+          :y2 (+  y 1)}])
+
+(defn box [x y]
+  [:rect
+   {:width 1
+    :height 1
+    :fill "grey"
+    :x x
+    :y y}])
+
+(defn stats []
+  [:div 
+   [:h1 (:text @state)]
+   [:h2 "blue"]
+   [:h1 (:x @state) (:y @state)]])
+
+(defn tile [x y]
+  [:g
+   (box x y)
+   (for [letter (seq (get-in @state [:board x y]))]
+     (case letter
+         "n" (border-top x y)
+         "s" (border-bottom x y)
+         "w" (border-left x y)
+         "e" (border-right x y)
+         nil))
+   (if 
+       (and 
+        (= x (:x @state))
+        (= y (:y @state)))
+     (circle x y))])
+
+(defn warren []
+  [:center
+   [:h1 (:text @state)]
+   (let [x (:x @state)
+        y (:y @state)]
+     [:h2 "X:" x " Y:" y " Tile:" (get-in @state [:board x y])])   
+   (into
+    [:svg
+     {:view-box (str "0 0 "  (first board-size) " " (second board-size))
+      :width (* 20 (first board-size))
+      :height (* 20 (second board-size))
+      :style {:border "1px solid black"}}] 
+    (for [i (range (first  board-size))
+          j (range (second  board-size))]
+      (tile i j)))])
+
+
+(reagent/render-component [warren]
+                          (. js/document (getElementById "app")))
+
+(defonce init
+  (.addEventListener js/window "keydown" #(handle-keys! %)))
+
+(defn on-js-reload []
+  (prn (:board @state)))
+
+ 
